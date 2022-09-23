7350c6a3f42eb16fbf9b1d859b60c7373a9aa6f5
Miguel O. Liu-Schiaffini <mliuschi@login1.cm.cluster>
2022-09-23 15:53:09 -0700

    Add lorenz and 2d fno models

---
+++ models/densenet.py
@@ -0,0 +1,31 @@
+import torch
+import numpy as np
+
+# A simple feedforward neural network
+class DenseNet(torch.nn.Module):
+    def __init__(self, layers, nonlinearity, out_nonlinearity=None, normalize=False):
+        super(DenseNet, self).__init__()
+
+        self.n_layers = len(layers) - 1
+
+        assert self.n_layers >= 1
+
+        self.layers = nn.ModuleList()
+
+        for j in range(self.n_layers):
+            self.layers.append(nn.Linear(layers[j], layers[j+1]))
+
+            if j != self.n_layers - 1:
+                if normalize:
+                    self.layers.append(nn.BatchNorm1d(layers[j+1]))
+
+                self.layers.append(nonlinearity())
+
+        if out_nonlinearity is not None:
+            self.layers.append(out_nonlinearity())
+
+    def forward(self, x):
+        for _, l in enumerate(self.layers):
+            x = l(x)
+
+        return x
---
+++ models/fno_2d.py
@@ -0,0 +1,145 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+import operator
+from functools import reduce
+
+import scipy.io
+
+import sys
+sys.path.append('../')
+from utilities import *
+
+torch.manual_seed(0)
+np.random.seed(0)
+
+
+def compl_mul2d(a, b):
+    # (batch, in_channel, x,y,t ), (in_channel, out_channel, x,y,t) -> (batch, out_channel, x,y,t)
+    return torch.einsum("bixy,ioxy->boxy", a, b)
+
+class SpectralConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1, modes2):
+        super(SpectralConv2d, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes1 #Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes2 = modes2
+
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+
+    def forward(self, x, size=None):
+        if size==None:
+            size = x.size(-1)
+
+        batchsize = x.shape[0]
+        #Compute Fourier coeffcients up to factor of e^(- something constant)
+        x_ft = torch.fft.rfftn(x, dim=[2,3], norm="ortho")
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.out_channels, size, size//2 + 1, device=x.device, dtype=torch.cfloat)
+        out_ft[:, :, :self.modes1, :self.modes2] = \
+            compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+        out_ft[:, :, -self.modes1:, :self.modes2] = \
+            compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
+
+
+        #Return to physical space
+        x = torch.fft.irfftn(out_ft, s=(size, size), dim=[2,3], norm="ortho")
+        return x
+
+class SimpleBlock2d(nn.Module):
+    def __init__(self, in_dim, out_dim, modes1, modes2, width):
+        super(SimpleBlock2d, self).__init__()
+
+        self.modes1 = modes1
+        self.modes2 = modes2
+
+        self.width_list = [width*2//4, width*3//4, width*4//4, width*4//4, width*5//4]
+        self.size_list = [64,] * 5
+        self.grid_dim = 2
+
+        self.fc0 = nn.Linear(in_dim+self.grid_dim, self.width_list[0])
+
+        self.conv0 = SpectralConv2d(self.width_list[0], self.width_list[1], self.modes1*4//4, self.modes2*4//4)
+        self.conv1 = SpectralConv2d(self.width_list[1], self.width_list[2], self.modes1*3//4, self.modes2*3//4)
+        self.conv2 = SpectralConv2d(self.width_list[2], self.width_list[3], self.modes1*2//4, self.modes2*2//4)
+        self.conv3 = SpectralConv2d(self.width_list[3], self.width_list[4], self.modes1*2//4, self.modes2*2//4)
+        self.w0 = nn.Conv1d(self.width_list[0], self.width_list[1], 1)
+        self.w1 = nn.Conv1d(self.width_list[1], self.width_list[2], 1)
+        self.w2 = nn.Conv1d(self.width_list[2], self.width_list[3], 1)
+        self.w3 = nn.Conv1d(self.width_list[3], self.width_list[4], 1)
+
+        self.fc1 = nn.Linear(self.width_list[4], self.width_list[4]*2)
+        self.fc2 = nn.Linear(self.width_list[4]*2, self.width_list[4]*2)
+        self.fc3 = nn.Linear(self.width_list[4]*2, out_dim)
+
+    def forward(self, x):
+
+        batchsize = x.shape[0]
+        size_x, size_y= x.shape[1], x.shape[2]
+        grid = self.get_grid(size_x, batchsize, x.device)
+        size_list = self.size_list
+
+        x = torch.cat((x, grid.permute(0, 2, 3, 1)), dim=-1)
+
+        x = self.fc0(x)
+        x = x.permute(0, 3, 1, 2)
+
+        x1 = self.conv0(x, size_list[1])
+        x2 = self.w0(x.view(batchsize, self.width_list[0], size_list[0]**2)).view(batchsize, self.width_list[1], size_list[0], size_list[0])
+        # x2 = F.interpolate(x2, size=size_list[1], mode='trilinear')
+        x = x1 + x2
+        x = F.selu(x) 
+
+        x1 = self.conv1(x, size_list[2])
+        x2 = self.w1(x.view(batchsize, self.width_list[1], size_list[1]**2)).view(batchsize, self.width_list[2], size_list[1], size_list[1])
+        # x2 = F.interpolate(x2, size=size_list[2], mode='trilinear')
+        x = x1 + x2
+        x = F.selu(x) 
+
+        x1 = self.conv2(x, size_list[3])
+        x2 = self.w2(x.view(batchsize, self.width_list[2], size_list[2]**2)).view(batchsize, self.width_list[3], size_list[2], size_list[2])
+        # x2 = F.interpolate(x2, size=size_list[3], mode='trilinear')
+        x = x1 + x2
+        x = F.selu(x)
+
+        x1 = self.conv3(x, size_list[4])
+        x2 = self.w3(x.view(batchsize, self.width_list[3], size_list[3]**2)).view(batchsize, self.width_list[4], size_list[3], size_list[3])
+        # x2 = F.interpolate(x2, size=size_list[4], mode='trilinear')
+        x = x1 + x2
+
+        x = x.permute(0, 2, 3, 1)
+        x = self.fc1(x)
+        x = F.selu(x)
+        x = self.fc2(x)
+        x = F.selu(x)
+        x = self.fc3(x)
+        return x
+
+    def get_grid(self, S, batchsize, device):
+        gridx = torch.tensor(np.linspace(0, 1, S), dtype=torch.float)
+        gridx = gridx.reshape(1, 1, S, 1).repeat([batchsize, 1, 1, S])
+        gridy = torch.tensor(np.linspace(0, 1, S), dtype=torch.float)
+        gridy = gridy.reshape(1, 1, 1, S).repeat([batchsize, 1, S, 1])
+        return torch.cat((gridx, gridy), dim=1).to(device)
+
+class Net2d(nn.Module):
+    def __init__(self, in_dim, out_dim, modes, width):
+        super(Net2d, self).__init__()
+        self.conv1 = SimpleBlock2d(in_dim, out_dim, modes, modes, width)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        return x
+
+    def count_params(self):
+        c = 0
+        for p in self.parameters():
+            c += reduce(operator.mul, list(p.size()))
+
+        return c
